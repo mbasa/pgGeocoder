bd5d39493969387875ab1c17bf147ce69092fc82
Mario Basa <mario.basa@gmail.com>
2018-07-28 14:46:13 +0800

    adding jsonb column to places to store misc data

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,16 +1,16 @@
-create table address_t (todofuken varchar(),lat float,lon float,
-    ttable varchar());
+create table address_t (todofuken varchar,lat float,lon float,
+    ttable varchar);
 
-create table address_s (todofuken varchar(),shikuchoson varchar(),
+create table address_s (todofuken varchar,shikuchoson varchar,
     lat float,lon float );
 
-create table address_o (todofuken varchar(),shikuchoson varchar(),
-    ooaza varchar(), tr_ooaza varchar(),
+create table address_o (todofuken varchar,shikuchoson varchar,
+    ooaza varchar, tr_ooaza varchar,
     lat float, lon float );
 
-create table address   (todofuken varchar(),shikuchoson varchar(),
-    ooaza varchar(), chiban varchar(),
+create table address   (todofuken varchar,shikuchoson varchar,
+    ooaza varchar, chiban varchar,
     lat float, lon float );
 
-create table places (id serial,owner varchar(),category varchar(), 
-    name varchar(),lat float, lon float);
+create table places (id serial,owner varchar,category varchar, 
+    name varchar,lat float, lon float);
