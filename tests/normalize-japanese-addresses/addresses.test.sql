58e615168efd09bbe26950b36619df3a433edd09
Ko Nagase <nagase@georepublic.co.jp>
2024-06-07 22:24:35 +0900

    Fix pg_prove error and adjust comment and begin timing

--- tests/normalize-japanese-addresses/addresses.test.sql
+++ tests/normalize-japanese-addresses/addresses.test.sql
@@ -1,3 +1,7 @@
+BEGIN;
+
+CREATE EXTENSION IF NOT EXISTS pgtap;
+
 CREATE TEMP TABLE normalize_japanese_addresses_test (
   input text,
   pref text,
@@ -8,13 +12,9 @@ CREATE TEMP TABLE normalize_japanese_addresses_test (
 
 \copy normalize_japanese_addresses_test FROM 'addresses.csv' WITH CSV HEADER;
 
-BEGIN;
-
-CREATE EXTENSION IF NOT EXISTS pgtap;
-
 SELECT plan(7104); -- SELECT count(*) FROM normalize_japanese_addresses_test
 
-DO $$
+CREATE FUNCTION test_addresses() RETURNS SETOF TEXT AS $$
 DECLARE
   data record;
   res record;
@@ -30,20 +30,21 @@ BEGIN
     INTO res
     FROM geocoder(data.input);
 
+    -- TODO: How and when check last other column ?
     SELECT array_to_string(ARRAY[res.pref, res.city, res.town], ',') INTO actual;
     SELECT array_to_string(ARRAY[data.pref, data.city, data.town], ',') INTO expected;
 
-    -- TODO: other column
-    SELECT is(
+    RETURN NEXT is(
       actual,
       expected,
       data.input
-    ) INTO result;
-    IF left(result, 3) = 'not' THEN
-      RAISE NOTICE '%', result;
-    END IF;
+    );
   END LOOP;
-END $$;
+  RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM test_addresses();
 
 SELECT * FROM finish();
 
