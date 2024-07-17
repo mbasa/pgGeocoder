bc4706aef2747057f28145f1feb288158facebfc
Ko Nagase <nagase@georepublic.co.jp>
2024-07-17 22:11:08 +0900

    Compress fixtures csv files

--- tests/dump_prod_tables_to_fixtures.sh
+++ tests/dump_prod_tables_to_fixtures.sh
@@ -11,3 +11,7 @@ source "${DIR}/.env"
 psql -U ${DBROLE} -d ${DBNAME} -c "\copy (SELECT todofuken, lat, lon, code, year FROM pggeocoder.address_t ORDER BY code, year) TO '${DIR}/fixtures/address_t.csv' WITH CSV HEADER;"
 psql -U ${DBROLE} -d ${DBNAME} -c "\copy (SELECT todofuken, shikuchoson, lat, lon, code, year FROM pggeocoder.address_s ORDER BY code, year) TO '${DIR}/fixtures/address_s.csv' WITH CSV HEADER;"
 psql -U ${DBROLE} -d ${DBNAME} -c "\copy (SELECT todofuken, shikuchoson, ooaza, lat, lon, code, year FROM pggeocoder.address_o ORDER BY code, year) TO '${DIR}/fixtures/address_o.csv' WITH CSV HEADER;"
+
+gzip "${DIR}/fixtures/address_t.csv"
+gzip "${DIR}/fixtures/address_s.csv"
+gzip "${DIR}/fixtures/address_o.csv"
