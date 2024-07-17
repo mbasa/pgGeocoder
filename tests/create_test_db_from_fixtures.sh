07163409205ac26695c6cf12592f882a4b6969a2
Ko Nagase <nagase@georepublic.co.jp>
2024-07-17 16:47:24 +0900

    Add year column to fixtures csv files

--- tests/create_test_db_from_fixtures.sh
+++ tests/create_test_db_from_fixtures.sh
@@ -24,9 +24,9 @@ psql -U ${DBROLE} -d ${TESTDBNAME} -f "${DIR}/../sql/createTables.sql"
 psql -U ${DBROLE} -d ${TESTDBNAME} -f "${DIR}/../sql/pgGeocoder.sql"
 psql -U ${DBROLE} -d ${TESTDBNAME} -f "${DIR}/../sql/pgReverseGeocoder.sql"
 
-psql -U ${DBROLE} -d ${TESTDBNAME} -c "\copy pggeocoder.address_t(todofuken, lat, lon, code) FROM '${DIR}/fixtures/address_t.csv' WITH CSV HEADER;"
-psql -U ${DBROLE} -d ${TESTDBNAME} -c "\copy pggeocoder.address_s(todofuken, shikuchoson, lat, lon, code) FROM '${DIR}/fixtures/address_s.csv' WITH CSV HEADER;"
-psql -U ${DBROLE} -d ${TESTDBNAME} -c "\copy pggeocoder.address_o(todofuken, shikuchoson, ooaza, lat, lon, code) FROM '${DIR}/fixtures/address_o.csv' WITH CSV HEADER;"
+psql -U ${DBROLE} -d ${TESTDBNAME} -c "\copy pggeocoder.address_t(todofuken, lat, lon, code, year) FROM '${DIR}/fixtures/address_t.csv' WITH CSV HEADER;"
+psql -U ${DBROLE} -d ${TESTDBNAME} -c "\copy pggeocoder.address_s(todofuken, shikuchoson, lat, lon, code, year) FROM '${DIR}/fixtures/address_s.csv' WITH CSV HEADER;"
+psql -U ${DBROLE} -d ${TESTDBNAME} -c "\copy pggeocoder.address_o(todofuken, shikuchoson, ooaza, lat, lon, code, year) FROM '${DIR}/fixtures/address_o.csv' WITH CSV HEADER;"
 
 psql -U ${DBROLE} -d ${TESTDBNAME} -c "UPDATE pggeocoder.address_t SET geog = ST_SetSRID(ST_MakePoint(lon, lat), 4326)::GEOGRAPHY;"
 psql -U ${DBROLE} -d ${TESTDBNAME} -c "UPDATE pggeocoder.address_s SET geog = ST_SetSRID(ST_MakePoint(lon, lat), 4326)::GEOGRAPHY;"
--- tests/dump_prod_tables_to_fixtures.sh
+++ tests/dump_prod_tables_to_fixtures.sh
@@ -8,6 +8,6 @@ DIR=$(cd $(dirname $0); pwd)
 source "${DIR}/../.env"
 source "${DIR}/.env"
 
-psql -U ${DBROLE} -d ${DBNAME} -c "\copy (SELECT todofuken, lat, lon, code FROM pggeocoder.address_t ORDER BY code) TO '${DIR}/fixtures/address_t.csv' WITH CSV HEADER;"
-psql -U ${DBROLE} -d ${DBNAME} -c "\copy (SELECT todofuken, shikuchoson, lat, lon, code FROM pggeocoder.address_s ORDER BY code) TO '${DIR}/fixtures/address_s.csv' WITH CSV HEADER;"
-psql -U ${DBROLE} -d ${DBNAME} -c "\copy (SELECT todofuken, shikuchoson, ooaza, lat, lon, code FROM pggeocoder.address_o ORDER BY code) TO '${DIR}/fixtures/address_o.csv' WITH CSV HEADER;"
+psql -U ${DBROLE} -d ${DBNAME} -c "\copy (SELECT todofuken, lat, lon, code, year FROM pggeocoder.address_t ORDER BY code, year) TO '${DIR}/fixtures/address_t.csv' WITH CSV HEADER;"
+psql -U ${DBROLE} -d ${DBNAME} -c "\copy (SELECT todofuken, shikuchoson, lat, lon, code, year FROM pggeocoder.address_s ORDER BY code, year) TO '${DIR}/fixtures/address_s.csv' WITH CSV HEADER;"
+psql -U ${DBROLE} -d ${DBNAME} -c "\copy (SELECT todofuken, shikuchoson, ooaza, lat, lon, code, year FROM pggeocoder.address_o ORDER BY code, year) TO '${DIR}/fixtures/address_o.csv' WITH CSV HEADER;"
