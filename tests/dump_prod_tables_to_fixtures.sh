f6fad1d58267c8b1fef029efd3918e1e4f839348
Ko Nagase <nagase@georepublic.co.jp>
2024-07-18 23:38:15 +0900

    Overwrite fixtures csv and gz without prompt

--- tests/dump_prod_tables_to_fixtures.sh
+++ tests/dump_prod_tables_to_fixtures.sh
@@ -12,6 +12,6 @@ psql -U ${DBROLE} -d ${DBNAME} -c "\copy (SELECT todofuken, lat, lon, code, year
 psql -U ${DBROLE} -d ${DBNAME} -c "\copy (SELECT todofuken, shikuchoson, lat, lon, code, year FROM pggeocoder.address_s ORDER BY code, year) TO '${DIR}/fixtures/address_s.csv' WITH CSV HEADER;"
 psql -U ${DBROLE} -d ${DBNAME} -c "\copy (SELECT todofuken, shikuchoson, ooaza, lat, lon, code, year FROM pggeocoder.address_o ORDER BY code, year) TO '${DIR}/fixtures/address_o.csv' WITH CSV HEADER;"
 
-gzip "${DIR}/fixtures/address_t.csv"
-gzip "${DIR}/fixtures/address_s.csv"
-gzip "${DIR}/fixtures/address_o.csv"
+gzip -f "${DIR}/fixtures/address_t.csv"
+gzip -f "${DIR}/fixtures/address_s.csv"
+gzip -f "${DIR}/fixtures/address_o.csv"
